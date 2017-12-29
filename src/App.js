6bab87a669bbcc3a87f337bf55b9c5a2002de2a7
Paulo Belo <pjbelo@gmail.com>
2017-12-29 21:15:52 +0000

    v0.59

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common'
 
+
 class App extends Component {
+
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: "AIzaSyDGpXU_CHvo3MHkbj9KapbkCJWqotL4TWo",
+            authDomain: "auth-pjbelo.firebaseapp.com",
+            databaseURL: "https://auth-pjbelo.firebaseio.com",
+            projectId: "auth-pjbelo",
+            storageBucket: "auth-pjbelo.appspot.com",
+            messagingSenderId: "810708015168"
+        });
+    }
+
     render() {
         return (
             <View>
